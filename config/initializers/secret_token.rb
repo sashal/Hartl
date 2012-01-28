3a0a2477c5c6ab50edc393f74f319c67e94b50c0
Sasha Lubomirsky <sashalubom@gmail.com>
2012-01-27 22:16:45 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlApp::Application.config.secret_token = '51de85185232668afdc9302572efdb28c3ce9c74ea533d664c91c99e2347c45c8281d2763aa867da3d35591f2d7480c3029051acc66cff366dbd1cc2ebc0050e'
